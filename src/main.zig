8a6dae2eb2adb1c13b31ca3402abf37cf06f95d9
Guillaume Ballet <gballet@gmail.com>
2020-12-15 10:52:29 +0100

    boolean serialization

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,14 @@ fn serialize(comptime T: type, data: T, l: *ArrayList(u8)) !void {
                     inline while (i < N) : (i += 1) {
                         const byte: u8 = @truncate(u8, data >> (8 * i));
                         try l.append(byte);
+                    }
+        },
+        .Bool => {
+            if (data) {
+                try l.append(1);
+            } else {
+                try l.append(0);
+            }
         },
         else => {
             return error.UnknownType;
@@ -50,4 +58,24 @@ test "serializes uint32" {
     defer list.deinit();
     try serialize(u32, data, &list);
     expect(std.mem.eql(u8, list.items, exp));
+}
+
+test "serializes bool" {
+    var data = false;
+    var serialized_data = [_]u8{0x00};
+    var exp = serialized_data[0..serialized_data.len];
+
+    var list = ArrayList(u8).init(std.testing.allocator);
+    defer list.deinit();
+    try serialize(bool, data, &list);
+    expect(std.mem.eql(u8, list.items, exp));
+
+    data = true;
+    serialized_data = [_]u8{0x01};
+    exp = serialized_data[0..serialized_data.len];
+
+    var list2 = ArrayList(u8).init(std.testing.allocator);
+    defer list2.deinit();
+    try serialize(bool, data, &list2);
+    expect(std.mem.eql(u8, list2.items, exp));
 }
