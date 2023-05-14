993ae0f5d7b596c78c9e4fdd1b9ff8ea4a839a94
Ignacio Hagopian <jsign.uy@gmail.com>
2023-05-14 11:50:10 -0300

    build: fix breaking changes from v0.11
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- build.zig
+++ build.zig
@@ -1,13 +1,13 @@
+const std = @import("std");
+const FileSource = std.build.FileSource;
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("ssz.zig", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const optimize = b.standardOptimizeOption(.{});
+    const lib = b.addStaticLibrary(.{ .name = "ssz.zig", .root_source_file = FileSource{ .path = "src/main.zig" }, .optimize = optimize, .target = .{} });
+    b.installArtifact(lib);
 
-    var main_tests = b.addTest("src/tests.zig");
-    main_tests.setBuildMode(mode);
+    var main_tests = b.addTest(std.Build.TestOptions{ .root_source_file = FileSource{ .path = "src/tests.zig" }, .optimize = optimize });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
