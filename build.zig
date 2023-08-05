f967c0ad29a45be68572afa80d1390e1d08370c1
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2023-08-05 18:47:54 +0200

    upgrade to 0.11.0

--- build.zig
+++ build.zig
@@ -1,14 +1,28 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("ssz.zig", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const lib = b.addStaticLibrary(.{
+        .name = "ssz",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .optimize = optimize,
+        .target = target,
+    });
+    b.installArtifact(lib);
 
-    var main_tests = b.addTest("src/tests.zig");
-    main_tests.setBuildMode(mode);
+    var main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .optimize = optimize,
+        .target = target,
+    });
+    var tests_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/tests.zig" },
+        .optimize = optimize,
+        .target = target,
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&tests_tests.step);
 }
